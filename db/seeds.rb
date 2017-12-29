5af86d1bd9fed6782b9227a79c01b99b9d630b55
Danny Wu <wushuo1988@hotmail.com>
2017-12-28 20:41:58 -0800

    Installed faker gem and added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+ 
+ # Create Posts
+ 50.times do
+   Post.create!(
+     title:  Faker::Lorem.sentence,
+     body:   Faker::Lorem.paragraph
+   )
+ end
+ posts = Post.all
+ 
+ # Create Comments
+ 100.times do
+   Comment.create!(
+     post: posts.sample,
+     body: Faker::Lorem.paragraph
+   )
+ end
+ 
+ puts "Seed finished"
+ puts "#{Post.count} posts created"
+ puts "#{Comment.count} comments created"
